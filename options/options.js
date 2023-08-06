4f76fc85281718e7d3a75d15903cedfde99373b1
Crowdbotics <team@crowdbotics.com>
2023-08-06 19:48:05 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "secure-small-loans-42691",
+  url: "https://secure-small-loans-42691.botics.co",
+  api: "https://secure-small-loans-42691.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
